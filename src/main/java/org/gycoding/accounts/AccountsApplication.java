1afad2ab8edcacfab0edc1ac7cc49a5bacff11a6
srtoxyc <ivi1000vm@gmail.com>
2024-05-21 11:00:14 +0200

    chore: add environment variables.

--- src/main/java/org/gycoding/accounts/AccountsApplication.java
+++ src/main/java/org/gycoding/accounts/AccountsApplication.java
@@ -1,13 +1,14 @@
 package org.gycoding.accounts;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AccountsApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AccountsApplication.class);
-        app.setRegisterShutdownHook(false);
-        app.run(args);
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(AccountsApplication.class, args);
     }
 }
