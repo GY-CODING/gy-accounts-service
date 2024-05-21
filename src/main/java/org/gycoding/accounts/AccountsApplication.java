891d6c5978f4bf37906201b508c2e21f0eee2a92
srtoxyc <ivi1000vm@gmail.com>
2024-05-21 11:32:38 +0200

    fix: fix environment variables management.

--- src/main/java/org/gycoding/accounts/AccountsApplication.java
+++ src/main/java/org/gycoding/accounts/AccountsApplication.java
@@ -7,8 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccountsApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-        SpringApplication.run(AccountsApplication.class, args);
+        try {
+            Dotenv dotenv = Dotenv.load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } catch(Exception e) {
+            System.out.println("Error loading .env file.");
+        } finally {
+            SpringApplication.run(AccountsApplication.class, args);
+        }
     }
 }
