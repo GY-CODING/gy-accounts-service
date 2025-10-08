0481aa1030523b6470d92a66a3c378f04a6a7b25
ToxYc <ivi1000vm@gmail.com>
2025-10-08 19:01:06 +0200

    fix
    Signed-off-by: ToxYc <ivi1000vm@gmail.com>

--- src/main/java/org/gycoding/accounts/application/dto/out/user/metadata/MetadataODTO.java
+++ src/main/java/org/gycoding/accounts/application/dto/out/user/metadata/MetadataODTO.java
@@ -2,11 +2,7 @@ package org.gycoding.accounts.application.dto.out.user.metadata;
 
 import lombok.*;
 
-@Data
 @Builder
-@Getter
-@Setter
-@AllArgsConstructor
-public class MetadataODTO {
-    private ProfileODTO profile;
-}
+public record MetadataODTO(
+        ProfileODTO profile
+) { }
